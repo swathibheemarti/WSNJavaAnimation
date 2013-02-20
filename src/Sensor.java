c0290468fe7e9bd8f2b39603aa816fb33db12858
swathi@bheemarthis.com <swathi@bheemarthis.com>
2013-02-20 01:43:36 -0500

    Drawing grid and placing random sensors....Swathi

---
+++ src/Sensor.java
@@ -0,0 +1,49 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+import java.util.*;
+
+public class Sensor {
+	
+	PApplet p;
+	PImage imgSensor;
+	int noOfSensors;
+	int[] sensorX, sensorY; //Sensors Location Array
+	
+	
+	public Sensor(PApplet _p, int _noOfSensors){
+		p = _p;
+		sensorX = new int[_noOfSensors];
+		sensorY = new int[_noOfSensors];
+		noOfSensors = _noOfSensors;
+		imgSensor = p.loadImage("sensor.jpg"); 
+	}
+	
+	/*
+	  xGrid and yGrid are generated randomly, 
+	  xGrid, yGrid represent random cell (grid) for placement of sensor
+	  x & y co-ordinate is calculated based onthe grid locatin as 
+	  x = gridnumber * size of grid + (som value to place the sensor middle of the grid)
+	*/
+
+	public void setupSensorLocations(int w, int h, int gridSize){
+	  int xGrid = 0, yGrid = 0;
+	  
+	  Random rn = new Random();
+	  
+	  for(int s = 0; s < noOfSensors; s++){
+	    
+	    xGrid = rn.nextInt(w/gridSize);
+	    yGrid = rn.nextInt(h/gridSize);
+	    
+	    sensorX[s] = (xGrid * gridSize) + (gridSize / 4);
+	    sensorY[s] = (yGrid * gridSize) + (gridSize / 4); 
+	  }
+	}
+	
+	public void drawSensors(){
+		for(int s = 0; s < noOfSensors; s++){     
+		     p.image(imgSensor, sensorX[s], sensorY[s], 10, 10);
+	     }
+	}
+	
+}
--- src/WSNAnimation.java
+++ src/WSNAnimation.java
@@ -3,10 +3,11 @@ import processing.core.*;
 public class WSNAnimation extends PApplet {
 
 	ImportAgentsDataFromExcel objAgentDataImport;
+	Sensor sensor;
 	
 	int w = 600, h = 600,            //Screen Size  
 	    gridSize = 40,               //Grid Size
-	    noOfSesnors = 40,            //No of Sensors watching agents 
+	    noOfSensors = 40,            //No of Sensors watching agents 
 	    t = 1,                       //Time variable
 	    noOfAgents = 200,            //No of agents -- this is coming from Matlab
 	    noOfAgentSets = 100,         //No of agent Sets (time laps)-- this is coming from Matlab
@@ -19,6 +20,18 @@ public class WSNAnimation extends PApplet {
 	//X, Y, S, D, T, p(x,y), q(s,d), s(p(x,y)), s(q(s,d)), Suspiciousness (0 or 1)
 	double AgentData[][][] = new double[noOfAgentSets][noOfAgents][noOfAgentProperties]; 
 	
+	int[] personX = new int[noOfAgents], personY = new int[noOfAgents],   //Agents Locations Array
+	      standingPersonX = new int[noOfStandingAgents], //Standing Agents Location Array
+	      standingPersonY = new int[noOfStandingAgents]; //Standing Agents Location Array
+
+	
+	
+	PImage[] imgPeople = new PImage[noOfAgents];  //Image for moving agents
+	PImage[] imgPeopleWalkingLeft = new PImage[noOfAgents]; //Image for agents moving opposite direction
+	PImage[] suspiciousAgents = new PImage[noOfAgents]; //Suspicious agent Image
+	PImage[] suspiciousAgentWalkingLeft = new PImage[noOfAgents]; //Suspicious agent Image
+	
+	PImage[] imgStandingPeople = new PImage[noOfStandingAgents]; //Image for standing agents
 	
 	/*
 	public static void main(String args[]){
@@ -28,18 +41,53 @@ public class WSNAnimation extends PApplet {
 	public void setup(){
 		
 		size(w, h);
-		objAgentDataImport = new ImportAgentsDataFromExcel(this, noOfAgentSets, noOfAgents, noOfAgentProperties, "AgentsData.xls");
+		LoadAgents();
+		SetupInitialSensorLocations();
 		
+	}
+	
+	public void LoadAgents(){
+		objAgentDataImport = new ImportAgentsDataFromExcel(this, noOfAgentSets, noOfAgents, noOfAgentProperties, "AgentsData.xls");
 		AgentData = objAgentDataImport.LoadAgentData();
 		
-		//Print DAta
-		objAgentDataImport.PrintAgentData();
+		//Print Data
+		//objAgentDataImport.PrintAgentData();
+	}
+	
+	public void SetupInitialSensorLocations(){
 		
+		sensor = new Sensor(this, noOfSensors);
+		
+		sensor.setupSensorLocations(w, h, gridSize);
 		
 	}
 	
 	public void draw(){
+		//Background is set to white
+		background(255);
 		
+		delay(100);
+		drawGrid();  
+		drawRandomSensors();  		  
 	}
-
+	
+	/*
+	  Grid lines are drawn horizontally and vertically
+	*/
+	public void drawGrid(){
+		//horizontal lines
+		for(int y = gridSize; y <= h; y += gridSize){
+			line(0, y, w, y);
+		}
+	  
+		//vertical lines
+		for(int x = 0; x <= w; x+=gridSize){
+			line(x, 0, x, h);
+		} 
+	}
+	
+	public void drawRandomSensors(){
+		sensor.drawSensors();
+	}
+	
 }
